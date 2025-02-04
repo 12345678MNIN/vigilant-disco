8839106bf29b6ea126bf9442ef0d531ad28b6c67
12345678MNIN <ooa665992@gmail.com>
2025-02-04 20:21:57 -0300

    ki.sh

--- ki.sh
+++ ki.sh
@@ -1,1 +1,23 @@
-ki
+#!/bin/bash
+
+echo "Welcome to the Linux command simulator!"
+PS3='Please enter your choice: '
+options=("List files" "Show date" "Check disk usage" "Exit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "List files")
+            ls -al
+            ;;
+        "Show date")
+            date
+            ;;
+        "Check disk usage")
+            df -h
+            ;;
+        "Exit")
+            break
+            ;;
+        *) echo "Invalid option $REPLY";;
+    esac
+done
